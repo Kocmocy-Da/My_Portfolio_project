2b757b692ec60d1f5544672fbbd7020664033a47
Zhenya Boyko <boykozhonya@yandex.com>
2023-01-23 20:01:31 +0300

    Added details for posts

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 def all_blogs(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-time')[:5] # последние 5 постов, сортировка по дате
     return render(request, 'blog/all_blogs.html', {'posts': posts})
+
+
+def detail(request, blogid):
+    blog = get_object_or_404(Post, pk=blogid)
+    return render(request, 'blog/detail.html', {'blog': blog})
